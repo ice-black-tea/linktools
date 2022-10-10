59522f6e4fbe9e6198278f15740a8dc0c757480d
胡霁 <jihu.hj@alibaba-inc.com>
2022-10-10 18:05:53 +0800

    fetch component enable flag from package manager

--- bow/framework/src/main/java/org/ironman/framework/bean/app/FComponent.java
+++ bow/framework/src/main/java/org/ironman/framework/bean/app/FComponent.java
@@ -1,8 +1,12 @@
 package org.ironman.framework.bean.app;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.pm.ComponentInfo;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageParser;
+
+import org.ironman.framework.Environment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +18,27 @@ public class FComponent<II extends PackageParser.IntentInfo> {
     public boolean enabled;
     public List<FIntentFilter> intents;
 
+    @SuppressLint("SwitchIntDef")
     public FComponent(PackageParser.Component<II> component, ComponentInfo info) {
-        name = new ComponentName(info.packageName, info.name).flattenToShortString();
+        PackageManager packageManager = Environment.getPackageManager();
+        ComponentName componentName = component.getComponentName();
+
+        name = componentName.flattenToShortString();
         exported = info.exported;
-        enabled = info.enabled;
+
+        switch (packageManager.getComponentEnabledSetting(componentName)) {
+            case PackageManager.COMPONENT_ENABLED_STATE_DEFAULT:
+                enabled = info.enabled;
+                break;
+            case PackageManager.COMPONENT_ENABLED_STATE_ENABLED:
+                enabled = true;
+                break;
+            case PackageManager.COMPONENT_ENABLED_STATE_DISABLED:
+            case PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER:
+            case PackageManager.COMPONENT_ENABLED_STATE_DISABLED_UNTIL_USED:
+                enabled = false;
+                break;
+        }
 
         if (component.intents != null && component.intents.size() > 0) {
             intents = new ArrayList<>(component.intents.size());
