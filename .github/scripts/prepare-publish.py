157394d457fa86253be5fa7a919a3bd6aefed6cd
胡霁 <jihu.hj@alibaba-inc.com>
2023-05-06 11:33:11 +0800

    update prepare-publish.py

--- .github/scripts/prepare-publish.py
+++ .github/scripts/prepare-publish.py
@@ -29,24 +29,26 @@
 import json
 import os
 import re
+import subprocess
 
 import yaml
 
 if __name__ == '__main__':
-    root_path = os.path.abspath(os.path.join(__file__, "..", "..", "..", "src", "linktools"))
+    root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
     ######################################################################
     # 处理版本号和release标志位
     ######################################################################
+    src_path = os.path.join(root_path, "src", "linktools")
     version = os.environ["VERSION"]
     if version.startswith("v"):
         version = version[len("v"):]
 
     version_patten = re.compile(r"^__version__\s+=\s*\"\S*\".*$")
     release_patten = re.compile(r"^__release__\s+=\s*\w+.*$")
-    with open(os.path.join(root_path, "version.py"), "rt") as fd:
+    with open(os.path.join(src_path, "version.py"), "rt") as fd:
         file_data = fd.read()
-    with open(os.path.join(root_path, "version.py"), "wt") as fd:
+    with open(os.path.join(src_path, "version.py"), "wt") as fd:
         for line in file_data.splitlines(keepends=True):
             result = line
             result = version_patten.sub(f"__version__ = \"{version}\"", result)
@@ -56,8 +58,26 @@ if __name__ == '__main__':
     ######################################################################
     # 将tools.yml转为tools.json
     ######################################################################
-    with open(os.path.join(root_path, "assets", "tools.yml"), "rb") as fd:
+    with open(os.path.join(src_path, "assets", "tools.yml"), "rb") as fd:
         file_data = yaml.safe_load(fd)
-    with open(os.path.join(root_path, "assets", "tools.json"), "wt") as fd:
+    with open(os.path.join(src_path, "assets", "tools.json"), "wt") as fd:
         json.dump(file_data, fd)
-    os.remove(os.path.join(root_path, "assets", "tools.yml"))
+    os.remove(os.path.join(src_path, "assets", "tools.yml"))
+
+    ######################################################################
+    # 编译frida.min.js
+    ######################################################################
+    subprocess.check_call(
+        "npm install && npm run build",
+        shell=True,
+        cwd=os.path.join(root_path, "agent", "frida")
+    )
+
+    ######################################################################
+    # 编译android-tools.apk
+    ######################################################################
+    subprocess.check_call(
+        "./gradlew --no-daemon :tools:buildTools",
+        shell=True,
+        cwd=os.path.join(root_path, "agent", "android")
+    )
