1dc847873c3e1571b52ac05c5d0761a526375b98
huji <669898595@qq.com>
2023-10-08 20:09:39 +0800

    fix bug

--- agent/frida/loader.ts
+++ agent/frida/loader.ts
@@ -12,7 +12,12 @@ export class ScriptLoader {
     load(scripts: Script[], parameters: Parameters) {
         for (const script of scripts) {
             try {
-                eval(script.source);
+                const name = `exec_${script.filename.replace(/[^A-Za-z0-9_]+/gi, "_")}`;
+                const func = (0, eval)(
+                    `(function ${name}(parameters) {${script.source}\n})\n` +
+                    `//# sourceURL=${script.filename}`
+                )
+                func(parameters);
             } catch (e) {
                 let message = e.hasOwnProperty("stack") ? e.stack : e;
                 throw new Error(`Unable to load ${script.filename}: ${message}`);
